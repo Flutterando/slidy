af59d6f72e01192e3760c9c8c57f05800141a68f
Anderson Mendes <anderson2000ano@gmail.com>
2019-06-25 10:29:44 -0300

    improve create start files and outputs

--- lib/src/modules/start.dart
+++ lib/src/modules/start.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:slidy/src/templates/templates.dart' as templates;
+import 'package:slidy/src/utils/file_utils.dart';
 import 'package:slidy/src/utils/output_utils.dart' as output;
 import 'package:slidy/src/utils/utils.dart';
 import 'package:slidy/src/modules/install.dart';
@@ -12,21 +13,19 @@ start(args) async {
     exit(1);
   }
 
+  output.msg("Starting a new project");
+
   String package = await getNamePackage();
 
-  File(dir.path + "/main.dart")
-    ..createSync()
-    ..writeAsStringSync(templates.startMain(package));
+  createStaticFile('${dir.path}/main.dart', templates.startMain(package));
 
-  File(dir.path + "/src/app_module.dart")
-    ..createSync(recursive: true)
-    ..writeAsStringSync(templates.startAppModule(package));
-  File(dir.path + "/src/app_bloc.dart")
-    ..createSync(recursive: true)
-    ..writeAsStringSync(templates.startAppBloc());
-  File(dir.path + "/src/app_widget.dart")
-    ..createSync(recursive: true)
-    ..writeAsStringSync(templates.startAppWidget(package));
+  createStaticFile(libPath('app_module.dart'), templates.startAppModule(package));
+
+  createStaticFile(libPath('app_bloc.dart'), templates.startAppBloc());
+
+  createStaticFile(libPath('app_widget.dart'), templates.startAppWidget(package));
+
   await install(["install", "bloc_pattern", "rxdart", "dio"]);
-  output.success("Project started! enjoy!");
+  
+  output.msg("Project started! enjoy!");
 }
