1d2711dd11bd9da0e3253dc3eb0c86b8a1672ae8
Jacob Moura <tecleigo@gmail.com>
2019-06-24 21:09:32 -0300

    add shortcut commands

--- bin/main.dart
+++ bin/main.dart
@@ -2,19 +2,22 @@ import 'package:slidy/slidy.dart';
 
 Map<String, Function> methods = {
   'start': (args) => start(args),
-  'generate': (args) => Generate(args), 'g': (args) => Generate(args),
+  'generate': (args) => Generate(args),
+  'g': (args) => Generate(args),
   'update': (args) => update(args),
   'upgrade': (args) => upgrade(),
   'install': (args) => install(args),
+  'i': (args) => install(args),
   'unistall': (args) => unistall(args),
   '--help': (args) => help(),
+  '-h': (args) => help(),
   '--version': (args) => version(),
-
+  '-v': (args) => version(),
 };
 
 main(List<String> args) async {
   if (args.isEmpty) {
-    print("Slidy");
+    version();
     return;
   }
 
