401e6beb039ec2051c843dd78c86ef426361c6f4
Marcelo Gustavo da Costa Reis <marcelogustavoreis@gmail.com>
2019-06-28 16:38:37 -0300

    ajuste no retorno do slidy upgrade

--- lib/src/modules/upgrade.dart
+++ lib/src/modules/upgrade.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
 import 'dart:io';
 
 void upgrade() {
   Process.runSync("pub", ["global", "activate", "slidy"], runInShell: true);
-  var process = Process.runSync("slidy", ["-v"], runInShell: true);
+  var process = Process.runSync("slidy", ["-v"], runInShell: true, stdoutEncoding: utf8);
   print(process.stdout);
 }
