37720c261bf6bf5dc2df4bf6524ca4a2c97c8385
Jacob Moura <jacobaraujo7@gmail.com>
2020-01-12 13:55:18 -0300

    add Modular.navigatorKey

--- lib/src/templates/start/src/app_widget.dart
+++ lib/src/templates/start/src/app_widget.dart
@@ -24,6 +24,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: Modular.navigatorKey,
       title: 'Flutter Slidy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
