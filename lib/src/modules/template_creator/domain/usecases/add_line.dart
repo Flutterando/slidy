6eddec7f7fe8eeb86d5fbbcc602dc61b467f6fda
Jacob Moura <jacobaraujo7@gmail.com>
2021-03-08 16:55:39 -0300

    result

--- lib/src/modules/template_creator/domain/usecases/add_line.dart
+++ lib/src/modules/template_creator/domain/usecases/add_line.dart
@@ -10,6 +10,6 @@ class AddLine extends UseCase<SlidyError, SlidyProccess, LineParams> {
     lines = params.replaceLine == null ? lines : lines.map<String>(params.replaceLine!).toList();
     lines.insertAll(params.position, params.inserts);
     await params.file.writeAsString(lines.join('\n'));
-    return Right(SlidyProccess(result: 'ok'));
+    return Right(SlidyProccess(result: '${params.file.uri.pathSegments.last} added line ${params.inserts.first}'));
   }
 }
