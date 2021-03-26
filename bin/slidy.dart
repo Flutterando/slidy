0ca2d7022cf9e64268bd3cef610727b9b7e66ee9
Jacob Moura <jacobaraujo7@gmail.com>
2021-03-26 17:12:46 -0300

    change version

--- bin/slidy.dart
+++ bin/slidy.dart
@@ -27,7 +27,7 @@ void executeOptions(ArgResults results, List<String> arguments, CommandRunner ru
   if (results.wasParsed('help') || arguments.isEmpty) {
     print(runner.usage);
   } else if (results.wasParsed('version')) {
-    version('3.0.1+1');
+    version('3.0.2');
   } else {
     print('Command not found!\n');
     print(runner.usage);
