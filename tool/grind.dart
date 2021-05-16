c51e39a8e477386f0a833d721d48e25093168174
Jacob Moura <jacobaraujo7@gmail.com>
2021-05-16 17:08:40 -0300

    update github name

--- tool/grind.dart
+++ tool/grind.dart
@@ -4,8 +4,8 @@ import 'package:cli_pkg/cli_pkg.dart' as pkg;
 void main(List<String> args) {
   pkg.name.value = 'slidy';
   pkg.humanName.value = 'slidy';
-  pkg.githubUser.value = 'flutterando';
-  pkg.homebrewRepo.value = 'flutterando/homebrew-slidy';
+  pkg.githubUser.value = 'Flutterando';
+  pkg.homebrewRepo.value = 'Flutterando/homebrew-slidy';
 
   pkg.addAllTasks();
   grind(args);
