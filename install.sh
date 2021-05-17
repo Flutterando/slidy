9a28afe874e6988ae35f9a7f9344a12368a09f06
allansrc <allanramos.teixeira@gmail.com>
2021-05-16 23:40:37 -0300

    hard specify arch

--- install.sh
+++ install.sh
@@ -12,10 +12,18 @@ cd $(pwd)/.temp_slidy
 THEARCH=$(uname -m)
 THEBIT=$(getconf LONG_BIT)
 
-if [[ $THEARCH == *$THEBIT* ]]; then
+if [[ $THEBIT == *64* ]]; then
 
   echo "downloading the lastest slidy' version!"
   THEURL=$(curl -s https://api.github.com/repos/Flutterando/slidy/releases | grep browser_download_url | grep 'linux-x64[.]tar[.]gz' | head -n 1 | cut -d '"' -f 4)
+  curl -L $THEURL > slidy_temp.tar.gz
+
+fi
+
+if [[ $THEBIT == *64* ]]; then
+
+  echo "downloading the lastest slidy' version!"
+  THEURL=$(curl -s https://api.github.com/repos/Flutterando/slidy/releases | grep browser_download_url | grep 'linux-ia32[.]tar[.]gz' | head -n 1 | cut -d '"' -f 4)
   curl -L $THEURL > slidy_temp.tar.gz
 
 fi
