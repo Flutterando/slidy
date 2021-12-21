af13eea1b6800cd9e8b3fa262c1e3d21cbb89b22
Bwolf <bwolfnoob@gmail.com>
2021-12-21 16:15:42 -0300

    Upp versions packages of example

--- example/test/app/modules/profile/profile_module_test.dart
+++ example/test/app/modules/profile/profile_module_test.dart
@@ -1,7 +1,6 @@
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:modular_test/modular_test.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:example/app/modules/profile/profile_module.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:modular_test/modular_test.dart';
 
 void main() {
   setUpAll(() {
