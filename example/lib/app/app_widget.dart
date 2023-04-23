d51d0cc076c6fb263b56b518e2fd0c6bbbe94ad0
Luis Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2023-04-22 22:46:23 -0300

    update dependencies

--- example/lib/app/app_widget.dart
+++ example/lib/app/app_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
